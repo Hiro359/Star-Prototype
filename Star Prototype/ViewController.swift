07ff558392d8c709f3dcfd774b56daa28f179d71
重信和宏 <kazushige359@gmail.com>
2016-11-18 20:19:41 +1100

    Initial Commit

---
+++ Star Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Prototype
+//
+//  Created by 重信和宏 on 18/11/16.
+//  Copyright © 2016 Hiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
